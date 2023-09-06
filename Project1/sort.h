770e26f3f31a1282b9a39728318b52b6596f1375
Pu Fanyi <pufanyi@gmail.com>
2023-09-06 21:55:20 +0800

    restructure

--- Project1/sort.h
+++ Project1/sort.h
@@ -7,9 +7,71 @@ typedef uint64_t compare_count_t;
 
 typedef compare_count_t (*SortFunction)(int *begin, const int *end, ...);
 
-compare_count_t insertionSort(int *begin, const int *end);
+compare_count_t insertionSort(int *begin, const int *end) {
+    compare_count_t compareCount = 0;
+    for (int *now = begin + 1; now != end; ++now) {
+        int key = *now;
+        int *p = now - 1;
+        while (p >= begin && ++compareCount && *p > key) {
+            *(p + 1) = *p;
+            --p;
+        }
+        *(p + 1) = key;
+    }
+    return compareCount;
+}
 
-compare_count_t mergeSort(int *begin, const int *end);
-compare_count_t mergeSortWithInsertionSort(int *begin, const int *end, int threshold);
+compare_count_t merge(int *begin, int *mid, const int *end) {
+    compare_count_t compareCount = 0;
+    int *left = begin;
+    int *right = mid;
+    size_t baseSize = sizeof(int) * (end - begin);
+    int *temp = (int *) malloc(baseSize);
+    int *p = temp;
+    while (left != mid && right != end) {
+        if (*left <= *right) {
+            *p++ = *left++;
+        } else {
+            *p++ = *right++;
+        }
+        ++compareCount;
+    }
+    while (left != mid) {
+        *p++ = *left++;
+    }
+    while (right != end) {
+        *p++ = *right++;
+    }
+    memcpy(begin, temp, baseSize);
+    free(temp);
+    return compareCount;
+}
+
+compare_count_t mergeSort(int *begin, const int *end) {
+    compare_count_t compareCount = 0;
+    size_t size = end - begin;
+    if (size == 1) {
+        return 0;
+    }
+    int *mid = begin + size / 2;
+    compareCount += mergeSort(begin, mid);
+    compareCount += mergeSort(mid, end);
+    compareCount += merge(begin, mid, end);
+    return compareCount;
+}
+
+compare_count_t mergeSortWithInsertionSort(int *begin, const int *end, int threshold) {
+    compare_count_t compareCount = 0;
+    size_t size = end - begin;
+    if (size <= threshold) {
+        compareCount += insertionSort(begin, end);
+    } else {
+        int *mid = begin + size / 2;
+        compareCount += mergeSortWithInsertionSort(begin, mid, threshold);
+        compareCount += mergeSortWithInsertionSort(mid, end, threshold);
+        compareCount += merge(begin, mid, end);
+    }
+    return compareCount;
+}
 
 #endif //PROJECT1_SORT_H
--- Project1/test_case_generator.h
+++ Project1/test_case_generator.h
@@ -1,10 +1,45 @@
 #ifndef PROJECT1_TEST_CASE_GENERATOR_H
 #define PROJECT1_TEST_CASE_GENERATOR_H
 
-void setUpRandom(void);
-int randInt(int min, int max);
-void randomShuffle(int *begin, const int *end);
-void generateRandomArray(int *begin, const int *end, int min, int max);
-void generateRandomPermutation(int *begin, const int *end);
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int randInt(int min, int max) {
+    if (max <= min) {
+        fprintf(stderr, "Error: Invalid range in randInt function.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return min + rand() % (max - min + 1);
+}
+
+void randomShuffle(int *begin, const int *end) {
+    size_t size = end - begin;
+    for (size_t i = 0; i < size; ++i) {
+        size_t j = i + rand() % (size - i);
+        int temp = begin[i];
+        begin[i] = begin[j];
+        begin[j] = temp;
+    }
+}
+
+void generateRandomArray(int *begin, const int *end, int min, int max) {
+    for (int *p = begin; p != end; ++p) {
+        *p = randInt(min, max);
+    }
+}
+
+void generateRandomPermutation(int *begin, const int *end) {
+    size_t size = end - begin;
+    for (size_t i = 0; i < size; ++i) {
+        begin[i] = (int) i;
+    }
+    randomShuffle(begin, end);
+}
+
+void setUpRandom() {
+    srand((unsigned int) time(NULL));
+}
 
 #endif //PROJECT1_TEST_CASE_GENERATOR_H
